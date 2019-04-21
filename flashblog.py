f294e39a7b1453fa7e286ac5c0b3d3040a8b1678
Listya <sikami>
2019-04-21 10:57:35 +0100

    another try

--- flashblog.py
+++ flashblog.py
@@ -3,4 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello ():
-    return "Hello, world"
+    return "<h1>Hello, world</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+if __name__ == '__main__':
+    app.run (debug = true)
