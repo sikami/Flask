3561ef6994ff78432bc2e9b87968dccfb18196c3
Listya <sikami>
2019-04-21 11:07:56 +0100

    I add additional words

--- flashblog.py
+++ flashblog.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello ():
-    return "<h1>Hello, world</h1>"
+    return "<h1>Hello, world. Why cant i display about page?</h1>"
 
 @app.route("/about")
-def about():
+def about ():
     return "<h1>About Page</h1>"
 
 if __name__ == '__main__':
-    app.run (debug = true)
+    app.run(debug=True)
