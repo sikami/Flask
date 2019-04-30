1744f315975be491c5cf3bb2ec2da69ff86d0948
Listya <sikami>
2019-04-30 08:50:55 +0100

    part 3

--- forms.py
+++ forms.py
@@ -1,2 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtf.validators import DataRequired, Length, Email, EqualTo
 
+class RegistrationForm(FlaskForm)
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=16)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm = PasswordField('confirm password', validators=[DataRequired(), EqualTo('pasword')])
 
