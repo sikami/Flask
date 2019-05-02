170f97d936e228023ec164f15226609fe5bd5622
Listya <sikami>
2019-05-02 17:09:55 +0100

    another commit

--- flashblog.py
+++ flashblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -33,8 +33,11 @@ def about ():
     return "<h1>About Page</h1>"
 
 @app.route("/register", methods=['GET', 'POST'])
-def registers ():
+def register ():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success') #need to create another template for this message
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2, max=16)])
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
-    confirm = PasswordField('confirm password', validators=[DataRequired(), EqualTo('pasword')])
+    confirm_password = PasswordField('confirm password', validators=[DataRequired(), EqualTo('pasword')])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
