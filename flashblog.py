fb328f10bab2891b0dafd3bbac1ae8c2b10d5cf9
Listya Widyasari <widyasari.listya@gmail.com>
2019-05-16 17:11:23 +0100

    blah

--- flashblog.py
+++ flashblog.py
@@ -36,7 +36,7 @@ def about ():
 def register ():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for { form.username.data }!', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
