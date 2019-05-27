ea27c0bcce105a9f36fc04161703f051fb967154
sikami <widyasari.listya@gmail.com>
2019-05-27 16:46:23 +0100

    another commit

--- application.py
+++ application.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd4746689083660e86ef9647cf8bacf3d'
+# URI database - next time change to heroku URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+#put class for database
+class User(db.model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = true, nullable = False)
+    email = db.Column(db.String(120), unique = true, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+
+    def __repr__(self):
+        return "User('{}', '{}', '{}'" .format(self.username, self.email, self.image_file)
+
+class Post(db.model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_post = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+
+    def __repr__(self):
+        return "Post({}, {})".format(self.title, self.date_post)
+#corey vid part 4, 14:59-ish
 
 posts = [ 
     {   'author': 'Listya W',
